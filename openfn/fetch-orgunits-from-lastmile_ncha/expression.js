b8f8b925fbd2c573c932bd4e9d704ec7c0f0e191
Dominic Surrao <dsurrao@hotmail.com>
2021-11-02 11:17:19 -0400

    added Grand Gedeh: Konobo, Gbao

--- openfn/fetch-orgunits-from-lastmile_ncha/expression.js
+++ openfn/fetch-orgunits-from-lastmile_ncha/expression.js
@@ -1,7 +1,8 @@
 query({
     sql: state => {
       return `select county, county_id, health_district_id, health_district, 
-      health_facility_id, health_facility, position_id as cha_position_id, chss_position_id 
-      from view_base_position_cha where county = 'Grand Bassa';`;
+        health_facility_id, health_facility, position_id as cha_position_id, 
+        chss_position_id from view_base_position_cha where county = 'Grand Bassa'
+        or (county = 'Grand Gedeh' and health_district in ('Konobo', 'Gbao'))`;
     }
 });
